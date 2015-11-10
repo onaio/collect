87b51bb49565dba01caa65ae2bd8bae3b646b4ba
Isaac Mwongela <imwongela@.ona.io>
2015-11-10 10:06:23 +0300

    IM : Prevent crash on null form version string

--- collect_app/src/main/java/io/ona/collect/android/activities/FormDownloadList.java
+++ collect_app/src/main/java/io/ona/collect/android/activities/FormDownloadList.java
@@ -623,10 +623,10 @@ public class FormDownloadList extends ListActivity implements FormListDownloader
             String jr_version = formCursor.getString(idxJrVersion);
             String formMd5Hash = "md5:" + formCursor.getString(idxMd5Hash);
 
-            Log.d("Form md5Hash ", md5Hash);
-            Log.d("Form formmd5Hash ", formMd5Hash);
+            Log.d("Form md5Hash ", ""+md5Hash);
+            Log.d("Form formmd5Hash ", ""+formMd5Hash);
             Log.d("Form version ", ""+latestVersion);
-            Log.d("Form formversion ", jr_version);
+            Log.d("Form formversion ", ""+jr_version);
 
             if ( formCursor.isNull(idxMd5Hash) ) {
                 // any non-null md5Hash on server is newer
