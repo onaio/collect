f36f9bd4b2cd41d9a7bf3225b583a0d019900b8a
Jason Rogena <jasonrogena@gmail.com>
2016-11-22 12:30:22 +0300

    Add Login Screen
    Add the Ona Collect login screen
    
    Fix for issue #33
    
    Signed-off-by: Jason Rogena <jasonrogena@gmail.com>

---
+++ collect_app/src/main/java/io/ona/collect/android/utils/DisplayUtils.java
@@ -0,0 +1,22 @@
+package io.ona.collect.android.utils;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+
+/**
+ * Created by Jason Rogena - jrogena@ona.io on 17/11/2016.
+ */
+
+public class DisplayUtils {
+    public static int dpToPx(Context context, int dp) {
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        int px = Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
+        return px;
+    }
+
+    public static int pxToDp(Context context, int px) {
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        int dp = Math.round(px / (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
+        return dp;
+    }
+}
