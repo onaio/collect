95d09561fccc37d26bbf723c91c22f9eb0be42c0
Jason Rogena <jasonrogena@gmail.com>
2016-12-20 16:40:54 +0300

    User and Forms MQTT Topics
    Add code for refreshing user's and forms' MQTT topics
    
    Signed-off-by: Jason Rogena <jasonrogena@gmail.com>

--- collect_app/src/main/java/io/ona/collect/android/utils/MQTTUtils.java
+++ collect_app/src/main/java/io/ona/collect/android/utils/MQTTUtils.java
@@ -2,6 +2,8 @@ package io.ona.collect.android.utils;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.Uri;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.preference.PreferenceManager;
@@ -18,9 +20,15 @@ import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.MqttSecurityException;
+import org.odk.collect.android.activities.FormDownloadList;
 import org.odk.collect.android.application.Collect;
+import org.odk.collect.android.listeners.FormListDownloaderListener;
+import org.odk.collect.android.logic.FormDetails;
 import org.odk.collect.android.logic.PropertyManager;
+import org.odk.collect.android.picasa.Content;
 import org.odk.collect.android.preferences.PreferencesActivity;
+import org.odk.collect.android.tasks.DownloadFormListTask;
+import org.odk.collect.android.utilities.WebUtils;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -30,11 +38,19 @@ import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManagerFactory;
+
+import io.ona.collect.android.R;
+import io.ona.collect.android.provider.FormsProviderAPI;
+import io.ona.collect.android.provider.InstanceProviderAPI;
 
 /**
  * Created by Jason Rogena - jrogena@ona.io on 09/12/2016.
@@ -60,8 +76,28 @@ public class MQTTUtils {
     private static final String CA_KEYSTORE_PASSWORD = "usTjh46qEvHeWmhTVB6CXQjFb";
     private static final String COLLECT_KEYSTORE_FILE = "android.collect.ona.io.bks";
     private static final String COLLECT_KEYSTORE_PASSWORD = "bU3mVNhcMHn8RwJBsKEdMBbpN";
+    private static final String TOPIC_ODK = "odk";
+    private static final String TOPIC_FORMS = "forms";
+    private static final String TOPIC_USERS = "users";
+    private static final String TOPIC_MESSAGES = "messages";
+    private static final String TOPIC_SCHEMA = "schema";
 
     private static MqttAndroidClient mqttAndroidClient;
+    private static HashMap<String, ArrayList<String>> subscribedFormTopics;
+    private static HashMap<String, ArrayList<String>> subscribedUserTopics;
+
+    static {
+        subscribedFormTopics = new HashMap<>();
+        subscribedFormTopics.put(TOPIC_SCHEMA, new ArrayList<String>());
+        subscribedFormTopics.put(TOPIC_MESSAGES, new ArrayList<String>());
+
+        subscribedUserTopics = new HashMap<>();
+        subscribedUserTopics.put(TOPIC_MESSAGES, new ArrayList<String>());
+    }
+
+    public static final int QOS_AT_MOST_ONCE = 0;
+    public static final int QOS_AT_LEAST_ONCE = 1;
+    public static final int QOS_EXACTLY_ONCE = 2;
 
     public static MqttAndroidClient getMqttAndroidClientInstance() {
         if(mqttAndroidClient == null) {
@@ -78,7 +114,7 @@ public class MQTTUtils {
             String clientId = getClientId();
 
             Log.d(TAG, "Client id is "+clientId);
-            mqttAndroidClient = new MqttAndroidClient(context, "ssl://10.20.22.150:8883", clientId);
+            mqttAndroidClient = new MqttAndroidClient(context, "ssl://10.20.22.169:8883", clientId);
             try {
                 Log.d(TAG, "About to try connection");
                 MqttConnectOptions options = new MqttConnectOptions();
@@ -134,7 +170,17 @@ public class MQTTUtils {
 
     private static void subscribeToAllTopics() {
         Log.i(TAG, "Subscribing to all topics");
-        subscribeToTopic("test", 2);
+
+        Context context = Collect.getInstance();
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
+        String username = settings.getString(PreferencesActivity.KEY_USERNAME, null);
+        String userMessagesTopic = getTopicString(new String[]{TOPIC_USERS, username, TOPIC_MESSAGES}, false);
+        ArrayList<String> newUserMessagesTopics = new ArrayList<>();
+        newUserMessagesTopics.add(userMessagesTopic);
+
+        updateSubscriptions(subscribedUserTopics.get(TOPIC_MESSAGES), newUserMessagesTopics, QOS_EXACTLY_ONCE);
+
+        refreshFormTopicSubscriptions(true);
     }
 
     /**
@@ -260,9 +306,10 @@ public class MQTTUtils {
         }
     }
 
-    public static void subscribeToTopic(final String[] topic, boolean subscribeToSubtopics, int qualityOfService) {
+    public static String subscribeToTopic(final String[] topic, boolean subscribeToSubtopics, int qualityOfService) {
         String topicString = getTopicString(topic, subscribeToSubtopics);
         subscribeToTopic(topicString, qualityOfService);
+        return topicString;
     }
 
     private static String getTopicString(String[] topic, boolean subscribeToSubtopics) {
@@ -288,17 +335,165 @@ public class MQTTUtils {
                 subToken.setActionCallback(new IMqttActionListener() {
                     @Override
                     public void onSuccess(IMqttToken asyncActionToken) {
-                        Log.d(TAG, "Successfully subscribed to MQTT topic: " + topic);
+                        Log.i(TAG, "Successfully subscribed to MQTT topic: " + topic);
                     }
                     @Override
                     public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
                         if (exception != null) exception.printStackTrace();
-                        Log.d(TAG, "Failed to subscribe to MQTT topic");
+                        Log.w(TAG, "Failed to subscribe to MQTT topic");
                     }
                 });
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static void unsubscribeFromTopic(final String topic) {
+        try {
+            if (mqttAndroidClient != null) {
+                IMqttToken unsubToken = mqttAndroidClient.unsubscribe(topic);
+                unsubToken.setActionCallback(new IMqttActionListener() {
+                    @Override
+                    public void onSuccess(IMqttToken asyncActionToken) {
+                        Log.i(TAG, "Successfully unsubscribed from MQTT topic " + topic);
+                    }
+
+                    @Override
+                    public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                        Log.w(TAG, "Could not subscribe to MQTT topic " + topic);
+                    }
+                });
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * This method subscribes to and unsubscribes from form topics depending on whether the forms are
+     * currently available on the device (should only subscribe to topics corresponding to forms that
+     * are currently on the device)
+     */
+    public static void refreshFormTopicSubscriptions(final boolean firstTime) {
+        DownloadFormListTask downloadFormListTask = new DownloadFormListTask();
+        downloadFormListTask.setDownloaderListener(new FormListDownloaderListener() {
+            @Override
+            public void formListDownloadingComplete(HashMap<String, FormDetails> value) {
+                if(value.containsKey(DownloadFormListTask.DL_AUTH_REQUIRED)) {
+                    if(firstTime) {
+                        Context context = Collect.getInstance().getApplicationContext();
+                        SharedPreferences settings =
+                                PreferenceManager.getDefaultSharedPreferences(context);
+                        String username =
+                                settings.getString(PreferencesActivity.KEY_USERNAME, null);
+                        String password =
+                                settings.getString(PreferencesActivity.KEY_PASSWORD, null);
+
+                        String server =
+                                settings
+                                        .getString(PreferencesActivity.KEY_SERVER_URL,
+                                                context.getResources()
+                                                        .getString(R.string.default_server_url));
+                        String formListUrl = context
+                                .getResources().getString(R.string.default_odk_formlist);
+                        final String url = server
+                                        + settings.getString(
+                                PreferencesActivity.KEY_FORMLIST_URL, formListUrl);
+                        Uri u = Uri.parse(url);
+                        WebUtils.addCredentials(username, password, u.getHost());
+                        refreshFormTopicSubscriptions(false);
+                    }
+                } else if(value.containsKey(DownloadFormListTask.DL_ERROR_MSG)) {
+                    Log.e(TAG, "An error occurred while trying to get the list of available forms");
+                } else {
+                    ArrayList<String> newFormSchemaSubscriptions = new ArrayList<String>();
+                    ArrayList<String> newFormMessagesSubscriptions = new ArrayList<String>();
+
+                    //get the Ona formId from the forms
+                    for(FormDetails curForm : value.values()) {
+                        //determine if form is currently downloaded
+                        if(isFormDownloaded(curForm.formID)) {
+                            String onaFormId = getOnaFormId(curForm.downloadUrl);
+                            Log.d(TAG, "Ona form id for "+curForm.downloadUrl+" is "+onaFormId);
+                            if(onaFormId != null) {
+                                String schemaTopicString = getTopicString(
+                                        new String[]{TOPIC_ODK,
+                                                TOPIC_FORMS,
+                                                onaFormId,
+                                                TOPIC_SCHEMA},
+                                        false);
+                                newFormSchemaSubscriptions.add(schemaTopicString);
+
+                                String messagesTopicString = getTopicString(
+                                        new String[]{TOPIC_ODK,
+                                                TOPIC_FORMS,
+                                                onaFormId,
+                                                TOPIC_MESSAGES},
+                                        false);
+                                newFormMessagesSubscriptions.add(messagesTopicString);
+                            }
+                        } else {
+                            Log.d(TAG, curForm.formID + " is not downloaded");
+                        }
+                    }
+
+                    updateSubscriptions(subscribedFormTopics.get(TOPIC_SCHEMA), newFormSchemaSubscriptions, QOS_EXACTLY_ONCE);
+                    updateSubscriptions(subscribedFormTopics.get(TOPIC_MESSAGES), newFormMessagesSubscriptions, QOS_EXACTLY_ONCE);
+                }
+            }
+        });
+        downloadFormListTask.execute();
+    }
+
+    private static void updateSubscriptions(ArrayList<String> currentSubscriptions, ArrayList<String> newSubscriptions, int qos) {
+        ArrayList<String> oldSubscriptions = new ArrayList<>(currentSubscriptions);
+        for(String curNewSubscription : newSubscriptions) {
+            if(oldSubscriptions.contains(curNewSubscription)) {
+                oldSubscriptions.remove(curNewSubscription);
+            } else {
+                subscribeToTopic(curNewSubscription, qos);
+                currentSubscriptions.add(curNewSubscription);
+            }
+
+            for(String curOldSubscription : oldSubscriptions) {
+                currentSubscriptions.remove(curOldSubscription);
+                unsubscribeFromTopic(curOldSubscription);
+            }
+        }
+    }
+
+    private static boolean isFormDownloaded(String jrFormId) {
+        String[] selectionArgs = { jrFormId };
+        String selection = FormsProviderAPI.FormsColumns.JR_FORM_ID + "=?";
+        String[] fields = { FormsProviderAPI.FormsColumns.JR_VERSION };
+
+        Cursor formCursor = null;
+        try {
+            formCursor = Collect.getInstance().getContentResolver().query(
+                    FormsProviderAPI.FormsColumns.CONTENT_URI,
+                    fields, selection, selectionArgs, null);
+            if ( formCursor.getCount() == 1 ) {
+                // form does not already exist locally
+                return true;
+            }
+        } finally {
+            if (formCursor != null) {
+                formCursor.close();
+            }
+        }
+
+        return false;
+    }
+
+    private static String getOnaFormId(String formDownloadUrl) {
+        //https://odk.ona.io/yri/forms/129132/form.xml
+        Pattern pattern = Pattern.compile("http[s]?://[\\w\\._]+/[\\w_]+/forms/([\\d]+)/form\\.xml");
+        Matcher matcher = pattern.matcher(formDownloadUrl);
+        if(matcher.find()) {
+            return matcher.group(1);
+        }
+
+        return null;
     }
 }
